9d17371398d4c82c07f721a8f3d0f59d815f907a
lijeffrey39 <lijeffrey39@gmail.com>
2018-01-14 15:05:36 -0500

    settings1

--- mysite/settings.py
+++ mysite/settings.py
@@ -128,7 +128,10 @@ STATIC_URL = '/static/'
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'staticfiles'))
+#STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'staticfiles'))
+
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+
+STATIC_ROOT = os.path.join(BASE_DIR, "live-static", "static-root")
